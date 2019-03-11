e9fa2ff449bd394f49ba862f1e3bb9ff0d4d158e
MM-Engineering1310 <mmengineering1310@gmail.com>
2019-03-11 11:33:55 -0400

    MS3

--- MS3_Code/src/main/java/CSV_To_DB.java
+++ MS3_Code/src/main/java/CSV_To_DB.java
@@ -20,16 +20,20 @@ public class CSV_To_DB
     }
 	public static void createT(Connection conn) // Creates the Table in using the SQLLite Library by passing in the connection argument from our Main Method
 	{
-        Statement st; // SQL Statement object used for executing any static SQL statement, we can use this initialized object to enokve other method and 
+        Statement st; // SQL Statement object used for executing any static SQL statement, we can use this initialized object to evoke other method and 
         // libraries of SQL and SQLLite
-        String query = "CREATE TABLE c(" 
+        String query = "CREATE TABLE c(" // Create the Table and store the information in a String, each column Header is made from the first row information
+        		// in our case the letters, and each column is further detailed by the SQL data type, since column G is decimal number it is the only one column
+        		// that does need to be stored as a text block and instead SQL will  process it as a DOUBLE, decimal type. Also Our CSV has no primary key
+        		// to uniquely ID the each row of data, so non was included. Which is not best practice. We would need to create a primary before parsing the CSV
+        		// or while making the Table.
                 + "A  TEXT   NOT NULL, "
                 + "B  TEXT   NOT NULL, "
                 + "C  TEXT   NOT NULL, "
                 + "D  TEXT   NOT NULL, "
                 + "E  TEXT   NOT NULL, "
                 + "F  TEXT   NOT NULL, "
-                + "G  INT   NOT NULL, "
+                + "G  DOUBLE   NOT NULL, "
                 + "H  TEXT   NOT NULL, "
                 + "I  TEXT   NOT NULL, "
                 + "J  TEXT   NOT NULL, "
@@ -37,20 +41,20 @@ public class CSV_To_DB
         System.out.println("QUERY: " + query);
         try 
         {
-            st = conn.createStatement();
-            st.executeUpdate(query);
+            st = conn.createStatement(); // Using our connection object, tells the SQLLite DB that we are creating an SQL statement object for sending commands to our DB
+            st.executeUpdate(query); // Update the statement object, which will update the SQL Database with the Table we just made
         } catch (SQLException e) { e.printStackTrace();} // Else throw an SQL exception, since our Object is a Database Object, and then 
         // print the printStackTrace of Stack Java.lang.Throwable class to print detail where and what type of error occurred      
    }
 	
-   public static void add_To_DB(Connection conn, String csv)
+   public static void add_To_DB(Connection conn, String csv) // Method for adding data by rows from the CSV to the DB, arguments passed are the connection object and our CSV
    {
-	    BufferedReader r = null;
-	    int jdbcbatch = 0;
-	    int tot = 0;
-	    PreparedStatement pt = null;
+	    BufferedReader r = null;  // Initialize a BufferReader request to to read a character or byte stream
+	    int jdbcbatch = 0;  
+	    int tot = 0; 
+	    PreparedStatement pt = null; // Initialize an SQL precomplied object and store that null object in a variable. We will use this object multiple times later
 	    
-	    String sql = "INSERT INTO c("
+	    String sql = "INSERT INTO c(" // Sets up a format for how each row of CSV information is to be added to the CSV, here we are using prepared statements to protect the data
                + "A, "
                + "B, "
                + "C, "
@@ -65,8 +69,9 @@ public class CSV_To_DB
 	    
 	    try 
 	    {
-	        pt = conn.prepareStatement(sql);
-	        r = new BufferedReader(new InputStreamReader(new FileInputStream(csv), "UTF-8"));
+	        pt = conn.prepareStatement(sql); // Use the connection object and tell SQLLite DB that we want to INSERT into our created table formated values.
+	        r = new BufferedReader(new InputStreamReader(new FileInputStream(csv), "UTF-8")); // Read in the raw CSV data from the file system using 
+	        // FileInputStream, then decode the bytes into the 8-bit Unicode character set before passing it to our BufferReader
 
             for(String line; (line = r.readLine()) != null;) 
             {
@@ -128,11 +133,12 @@ public class CSV_To_DB
    {
 
           String url = "jdbc:sqlite:" + path;
-          Connection conn = null;
+          Connection conn = null; //Set initial connection object to null before up updating later
           try 
           {
               Class.forName("org.sqlite.JDBC");
-              conn = DriverManager.getConnection(url);
+              conn = DriverManager.getConnection(url); //Update our connection object using the Driver Manager which is where JDBC Drivers used by their applications
+              // are found, basically it uses JDBC to establish a link with the DB
           } catch (Exception e) { e.printStackTrace(); }
           return conn;
    }
